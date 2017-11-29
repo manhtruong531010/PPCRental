8ca7f706d513845a5eefb1b2f4738a9f269a1f52
dinhtrungtin97 <dinhtrungtin97@gmail.com>
2017-11-29 13:45:32 +0700

    Login/Logout

--- Areas/Admins/Controllers/AdminController.cs
+++ Areas/Admins/Controllers/AdminController.cs
@@ -23,6 +23,97 @@ namespace Website_BĐS.Areas.Admins.Controllers
             var product = model.PROPERTies.ToList();
             return View(product);
         }
+        public ActionResult Create()
+        {
+            ReadList();
+            return View();
+        }
+        public class XulyModels
+        {
+            Team33Entities db = new Team33Entities();
+            public long Insert(PROPERTY entytiy)
+            {
+                db.PROPERTies.Add(entytiy);
+                db.SaveChanges();
+                return entytiy.ID;
+            }
+        }
+        [HttpPost]
+     
+        public ActionResult Create(PROPERTY pROPERTY, List<HttpPostedFileBase> files)
+        {
+            ReadList();
+            var product = new PROPERTY();
+
+            try
+            {
+
+                string filename = Path.GetFileNameWithoutExtension(pROPERTY.AvatarFile.FileName);
+                string extension = Path.GetExtension(pROPERTY.AvatarFile.FileName);
+                filename = filename + "checkcheck" + DateTime.Now.ToString("yymmssfff") + extension;
+                pROPERTY.Avatar = filename;
+                filename = Path.Combine(Server.MapPath("~/Images"), filename);
+                // Avatar
+
+                if (Path.GetFileNameWithoutExtension(pROPERTY.AvatarFile.FileName) == null)
+                {
+                    string s2 = "~/Images/ImagesNull.png";
+                    pROPERTY.AvatarFile.SaveAs(s2);
+                    //property.ImageFile2.SaveAs(filename2);
+                }
+                else
+                {
+                    //property.ImageFile2.SaveAs(filename2);
+                    pROPERTY.AvatarFile.SaveAs(filename);
+                }
+
+                pROPERTY.Created_at = DateTime.Parse(DateTime.Now.ToShortDateString());
+                var model = new XulyModels();
+                if (ModelState.IsValid)
+                {
+                    long id = model.Insert(pROPERTY);
+                    var path = "";
+                    foreach (var item in files)
+                    {
+                        if (item != null)
+                        {
+                            if (item.ContentLength > 0)
+                            {
+                                if (Path.GetExtension(item.FileName).ToLower() == ".jpg"
+                                    || Path.GetExtension(item.FileName).ToLower() == ".png"
+                                    || Path.GetExtension(item.FileName).ToLower() == ".gif"
+                                    || Path.GetExtension(item.FileName).ToLower() == ".jpeg")
+                                {
+                                    var path0 = id + item.FileName;
+                                    path = Path.Combine(Server.MapPath("~/MultiImages"), path0);
+
+                                    item.SaveAs(path);
+                                    ViewBag.UploadSuccess = true;
+
+                                }
+                            }
+                        }
+                    }
+                    if (id > 0)
+                    {
+                        return RedirectToAction("Index", "Agency");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Create khong thanh cong");
+                    }
+                }
+
+
+            }
+            catch
+            {
+
+            }
+
+
+            return View(pROPERTY);
+        }
         [HttpGet]
         public ActionResult Edit(int id)
         {
@@ -101,49 +192,9 @@ namespace Website_BĐS.Areas.Admins.Controllers
             return RedirectToAction("Details","Admin", new { id = ID});
 
         }
-        [HttpGet]
-       public ActionResult CreateNew()
-        { 
-            return View();
-        }
-      [HttpPost]
-        public ActionResult CreateNew(PROPERTY p)
-        {
-            //if(ModelState.IsValid){
-            //    model.PROPERTY.Add(p);
-            //    model.SaveChanges();
-            //    //if (p.AvatarFile != null && p.AvatarFile.ContentLength > 0)
-            //    //{
-            //    //    if (Path.GetExtension(p.AvatarFile.FileName).ToLower() == ".jpg"
-            //    //        || Path.GetExtension(p.AvatarFile.FileName).ToLower() == ".png"
-            //    //        || Path.GetExtension(p.AvatarFile.FileName).ToLower() == ".gif"
-            //    //        || Path.GetExtension(p.AvatarFile.FileName).ToLower() == ".jpeg")
-            //    //    {
-            //    //        string filename = Path.GetFileNameWithoutExtension(p.AvatarFile.FileName);
-            //    //        string extention = Path.GetExtension(p.AvatarFile.FileName);
-
-            //    //        filename = filename + DateTime.Now.ToString("yymmfff") + extention;
-            //    //        p.Avatar = filename;
-
-            //    //        filename = Path.Combine(Server.MapPath("~/Images"), filename);
-
-            //    //        p.AvatarFile.SaveAs(filename);
-                       
-            //        }
-         
-            //    return View(p);
-            if (ModelState.IsValid)
-            {
-                model.PROPERTies.Add(p);
-                model.SaveChanges();
-
-                return RedirectToAction("ViewProperty", "Admin");
-            }
-            else
-            {
-                return View(p);
-            }
-        }
+        
+            
+        
    }
 
 }
---
+++ Areas/Agency/Controllers/AgencyController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using Website_BĐS.Models;
+namespace Website_BĐS.Areas.Agency.Controllers
+{
+    public class AgencyController : Controller
+    {
+        Team33Entities model = new Team33Entities();
+        // GET: /Admin/ProductAdmin/
+        public ActionResult Index()
+        {
+            if (Session["userid"] != null)
+            {
+                var id = (int)Session["userid"];
+                var user = model.PROPERTies.Where(x => x.USER.ID == id).ToList();
+                return View(user);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
